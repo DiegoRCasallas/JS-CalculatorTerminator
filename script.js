fc08f96e7881cf9cc0c54024a49c4e9ebecb541f
juan camacho <lemejia11127@gmail.com>
2023-09-26 11:33:32 -0500

    modulos combinatorios,modulos nicolas

--- script.js
+++ script.js
@@ -42,5 +42,102 @@ function calculate() {
 //Funciones Esteban
 
 //Funciones Camilo
+function permutacionesSinRepeticion(n, r) {
+    if (n < r) {
+        return 0; 
+    }
+    function factorial(num) {
+        if (num <= 1) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+    const numerador = factorial(n);
+    const denominador = factorial(n - r);
+    return numerador / denominador;
+    }
+const permutaciones1 = permutacionesSinRepeticion(n, r);
+console.log(permutaciones);
+
+function permutacionesConRepeticion(n, r) {
+    return Math.pow(n, r);
+    }
+const permutaciones2 = permutacionesConRepeticion(n, r);
+console.log(permutaciones);
+
+function combinacionesSinRepeticion(n, r) {
+    if (n < r) {
+        return 0; 
+    }
+    function factorial(num) {
+        if (num <= 1) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+    const numerador = factorial(n);
+    const denominador = factorial(r) * factorial(n - r);
+    return numerador / denominador;
+}
+const combinaciones1 = combinacionesSinRepeticion(n, r);
+console.log(combinaciones);
+
+function combinacionesConRepeticion(n, r) {
+    function factorial(num) {
+        if (num <= 1) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+    const numerador = factorial(n + r - 1);
+    const denominador = factorial(r) * factorial(n - 1);
+    return numerador / denominador;
+    }
+    const combinaciones2 = combinacionesConRepeticion(n, r);
+    console.log(combinaciones);
 
 // Funciones Nicolas
+function Euler(a, n) {
+    function calcularEuler(n) {
+        let phi = 1;
+        for (let i = 2; i < n; i++) {
+            if (gcd(i, n) === 1) {
+                phi++;
+            }
+        }
+        return phi;
+    }
+
+    function gcd(a, b) {
+        if (b === 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    const phiN = calcularEuler(n);
+    const resultado = BInt(a) ** BInt(phiN) % BInt(n);
+
+    return resultado.toString();
+}
+
+// Función para calcular la secuencia de Fibonacci
+function Fibonacci(inicio, saltos) {
+    const fibonacciSecuencia = [inicio];
+    let numAnterior = inicio;
+    let numActual = inicio;
+
+    for (let i = 1; i < saltos; i++) {
+        const siguienteNumero = numAnterior + numActual;
+        fibonacciSecuencia.push(siguienteNumero);
+        numAnterior = numActual;
+        numActual = siguienteNumero;
+    }
+
+    return fibonacciSecuencia;
+}
+
+// Función para verificar la congruencia entre dos términos
+function sonCongruentes(a, b, m) {
+    return a % m === b % m;
+}
