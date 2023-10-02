5688a1a19e8a822060b48f31a9d1cb3376128327
Mr-Nl <josenls02@gmail.com>
2023-10-02 17:52:17 -0500

    Formato

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 let displayValue = "";
 
 //para usar con los inputs de arriba (los dos pequeños)
-let displayValue1="";
-let displayValue2="";
+let displayValue1 = "";
+let displayValue2 = "";
 
 function clearDisplay() {
     displayValue = "";
     document.getElementById("display").value = displayValue;
-    document.getElementById("display-1").value= displayValue;
-    document.getElementById("display-2").value= displayValue;
+    document.getElementById("display-1").value = displayValue;
+    document.getElementById("display-2").value = displayValue;
 }
 
 
@@ -24,12 +24,12 @@ function appendToDisplay(value) {
 
 function calculate() {
     try {
-       /* La línea `displayValue = eval(displayValue);` está evaluando el valor de la variable
-       `displayValue` como una expresión JavaScript. La función `eval()` toma un argumento de cadena
-       y lo evalúa como código JavaScript. En este caso, se trata de evaluar la expresión matemática
-       almacenada en "displayValue" y asignar el resultado nuevamente a "displayValue". */
+        /* La línea `displayValue = eval(displayValue);` está evaluando el valor de la variable
+        `displayValue` como una expresión JavaScript. La función `eval()` toma un argumento de cadena
+        y lo evalúa como código JavaScript. En este caso, se trata de evaluar la expresión matemática
+        almacenada en "displayValue" y asignar el resultado nuevamente a "displayValue". */
         displayValue = eval(displayValue);
-        
+
         document.getElementById("display").value = displayValue;
     } catch (error) {
         alert("Error en la expresión");
@@ -41,31 +41,31 @@ function calculate() {
 //Funciones Esteban
 function dividirEnteros(num1, num2) {
     if (num2 === 0) {
-      return "No es posible dividir por cero";
+        return "No es posible dividir por cero";
     }
     return Math.trunc(num1 / num2);
-  }
+}
 
-  function esPrimo(numero) {
+function esPrimo(numero) {
     if (numero <= 1) {
-      return false;
+        return false;
     }
     if (numero <= 3) {
-      return true;
+        return true;
     }
     if (numero % 2 === 0 || numero % 3 === 0) {
-      return false;
+        return false;
     }
     let i = 5;
     while (i * i <= numero) {
-      if (numero % i === 0 || numero % (i + 2) === 0) {
-        return false;
-      }
-      i += 6;
+        if (numero % i === 0 || numero % (i + 2) === 0) {
+            return false;
+        }
+        i += 6;
     }
     return true;
-  }
-  // Función para encontrar el MCD (Máximo Común Divisor)
+}
+// Función para encontrar el MCD (Máximo Común Divisor)
 function calcularMCD(a, b) {
     if (b === 0) {
         return a;
@@ -79,52 +79,52 @@ function calcularMCM(a, b) {
     // MCM(a, b) = (a * b) / MCD(a, b)
     return (a * b) / calcularMCD(a, b);
 }
- //---->METODOS PARA CALCULAR Y MOSTRAR EN PANTALLA (Pendiente)
- function mostrarEsPrimo(){
+//---->METODOS PARA CALCULAR Y MOSTRAR EN PANTALLA (Pendiente)
+function mostrarEsPrimo() {
     try {
 
-         displayValue = esPrimo(displayValue);
-         document.getElementById("display").value = displayValue;
+        displayValue = esPrimo(displayValue);
+        document.getElementById("display").value = displayValue;
 
-         
-     } catch (error) {
-         alert("Error en la expresión");
-         clearDisplay();
-     }
-}
 
-function mostrarDivInt(){
-    try {
-        
-        num2=document.getElementById("display-2").value;
-        num1=document.getElementById("display-1").value;
-
-        document.getElementById("display").value = dividirEnteros(num1,num2);
-         
-     } catch (error) {
-         alert("Error en la expresión");
-         clearDisplay();
-     }
-}
-function mostrarMCD(){
-    try {
-
-        num1 = document.getElementById("display-1").value;
-        num2 = document.getElementById("display-2").value;
-
-        document.getElementById("display").value = calcularMCD(num1,num2)
-    } catch (error){
+    } catch (error) {
         alert("Error en la expresión");
         clearDisplay();
     }
 }
-function mostrarMCM(){
-    try{
+
+function mostrarDivInt() {
+    try {
+
+        num2 = document.getElementById("display-2").value;
+        num1 = document.getElementById("display-1").value;
+
+        document.getElementById("display").value = dividirEnteros(num1, num2);
+
+    } catch (error) {
+        alert("Error en la expresión");
+        clearDisplay();
+    }
+}
+function mostrarMCD() {
+    try {
+
         num1 = document.getElementById("display-1").value;
         num2 = document.getElementById("display-2").value;
 
-        document.getElementById("display").value = calcularMCM(num1,num2)
-    } catch (error){
+        document.getElementById("display").value = calcularMCD(num1, num2)
+    } catch (error) {
+        alert("Error en la expresión");
+        clearDisplay();
+    }
+}
+function mostrarMCM() {
+    try {
+        num1 = document.getElementById("display-1").value;
+        num2 = document.getElementById("display-2").value;
+
+        document.getElementById("display").value = calcularMCM(num1, num2)
+    } catch (error) {
         alert("Error en la expresión");
         clearDisplay();
     }
@@ -133,7 +133,7 @@ function mostrarMCM(){
 //permutacion sin repeticion
 function permutacionesSinRepeticion(n, r) {
     if (n < r) {
-        return 0; 
+        return 0;
     }
     function factorial(num) {
         if (num <= 1) {
@@ -144,37 +144,37 @@ function permutacionesSinRepeticion(n, r) {
     const numerador = factorial(n);
     const denominador = factorial(n - r);
     return numerador / denominador;
+}
+function mostrar_nPr() {
+    try {
+        let r = document.getElementById("display-2").value;
+        let n = document.getElementById("display-1").value;
+        document.getElementById("display").value = permutacionesSinRepeticion(n, r);
+    } catch (error) {
+        alert("Error en la expresión");
+        clearDisplay();
     }
-function mostrar_nPr(){
-    try { 
-        let r=document.getElementById("display-2").value;
-        let n=document.getElementById("display-1").value;
-        document.getElementById("display").value = permutacionesSinRepeticion(n,r);
-        } catch (error) {
-            alert("Error en la expresión");
-            clearDisplay();
-        }
 }
 
 //permutacion con repeticion
 function permutacionesConRepeticion(n, r) {
     return Math.pow(n, r);
+}
+function mostrar_nPr_R() {
+    try {
+        r = document.getElementById("display-2").value;
+        n = document.getElementById("display-1").value;
+        document.getElementById("display").value = permutacionesConRepeticion(n, r);
+    } catch (error) {
+        alert("Error en la expresión");
+        clearDisplay();
     }
-function mostrar_nPr_R(){
-    try { 
-        r=document.getElementById("display-2").value;
-        n=document.getElementById("display-1").value;
-        document.getElementById("display").value = permutacionesConRepeticion(n,r);
-        } catch (error) {
-            alert("Error en la expresión");
-            clearDisplay();
-        }
 }
 
 //combinacion sin repeticion
 function combinacionesSinRepeticion(n, r) {
     if (n < r) {
-        return 0; 
+        return 0;
     }
     function factorial(num) {
         if (num <= 1) {
@@ -186,15 +186,15 @@ function combinacionesSinRepeticion(n, r) {
     const denominador = factorial(r) * factorial(n - r);
     return numerador / denominador;
 }
-function mostrar_nCr(){
+function mostrar_nCr() {
     try {
-        r=document.getElementById("display-2").value;
-        n=document.getElementById("display-1").value;
-        document.getElementById("display").value = combinacionesSinRepeticion(n,r);
-        } catch (error) {
-            alert("Error en la expresión");
-            clearDisplay();
-        }
+        r = document.getElementById("display-2").value;
+        n = document.getElementById("display-1").value;
+        document.getElementById("display").value = combinacionesSinRepeticion(n, r);
+    } catch (error) {
+        alert("Error en la expresión");
+        clearDisplay();
+    }
 }
 
 //combinacion con repeticion
@@ -208,16 +208,16 @@ function combinacionesConRepeticion(n, r) {
     const numerador = factorial(n + r - 1);
     const denominador = factorial(r) * factorial(n - 1);
     return numerador / denominador;
+}
+function mostrar_nCr_R() {
+    try {
+        r = document.getElementById("display-2").value;
+        n = document.getElementById("display-1").value;
+        document.getElementById("display").value = combinacionesConRepeticion(n, r);
+    } catch (error) {
+        alert("Error en la expresión");
+        clearDisplay();
     }
-function mostrar_nCr_R(){
-    try { 
-        r=document.getElementById("display-2").value;
-        n=document.getElementById("display-1").value;
-        document.getElementById("display").value = combinacionesConRepeticion(n,r);
-        } catch (error) {
-            alert("Error en la expresión");
-            clearDisplay();
-        }
 }
 //---->METODOS PARA CALCULAR Y MOSTRAR EN PANTALLA (Pendiente)
 
@@ -227,11 +227,11 @@ function mostrar_nCr_R(){
 function secuenciaEuler(n) {
     let sum = 0;
     for (let i = 0; i < n; i++) {
-      sum += Math.pow(1 + 1 / n, n);
+        sum += Math.pow(1 + 1 / n, n);
     }
     return sum;
-  }
-  
+}
+
 // Función para calcular la secuencia de Fibonacci
 function Fibonacci(inicio, saltos) {
     const fibonacciSecuencia = [inicio];
@@ -250,29 +250,29 @@ function Fibonacci(inicio, saltos) {
 
 // Función para verificar la congruencia entre dos términos y un modulo dado
 
-function sonCongruentes(a,b,mod){
+function sonCongruentes(a, b, mod) {
     let resultadoA = a % mod;
-    let resultadoB= b % mod; 
+    let resultadoB = b % mod;
     return resultadoA === resultadoB;
 
 }
 
 //--->METODOS PARA CALCULAR Y MOSTRAR EN PANTALLA (Pendiente)
 
-function mostrarFibonacci(){
+function mostrarFibonacci() {
     try {
-        let num1=parseInt(document.getElementById("display-1").value);
-        let num2=parseInt(document.getElementById("display-2").value);
-        document.getElementById("display").value = Fibonacci(num1,num2);
+        let num1 = parseInt(document.getElementById("display-1").value);
+        let num2 = parseInt(document.getElementById("display-2").value);
+        document.getElementById("display").value = Fibonacci(num1, num2);
     } catch (error) {
         alert("Error en la expresión");
         clearDisplay();
     }
 }
 
-function mostrarEuler(){
+function mostrarEuler() {
     try {
-        let num=parseInt(document.getElementById("display").value);
+        let num = parseInt(document.getElementById("display").value);
         document.getElementById("display").value = secuenciaEuler(num);
     } catch (error) {
         alert("Error en la expresión");
@@ -280,12 +280,12 @@ function mostrarEuler(){
     }
 }
 
-function mostrarCongruencia(){
+function mostrarCongruencia() {
     try {
         let num1 = parseInt(document.getElementById("display-1").value);
         let num2 = parseInt(document.getElementById("display-2").value);
         let mod = parseInt(document.getElementById("display").value);
-        document.getElementById("display").value =sonCongruentes(num1, num2,mod);
+        document.getElementById("display").value = sonCongruentes(num1, num2, mod);
     } catch (error) {
         alert("Error en la expresión");
         clearDisplay();
